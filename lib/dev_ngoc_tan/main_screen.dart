ce9318f8019e61945dc67c48891abfbc11bcc335
TylerDo <ngoctan2512@gmail.com>
2021-11-09 09:21:19 +0700

    update tấn

--- lib/dev_ngoc_tan/main_screen.dart
+++ lib/dev_ngoc_tan/main_screen.dart
@@ -10,7 +10,7 @@ class MainScreen extends StatelessWidget {
         title: const Text("Dev Ngoc Tuan Screen"),
       ),
       body: const Center(
-        child: Text("Hello Ngoc Tan do nhé.uhhuhu"),
+        child: Text("duoc rồi nhé em"),
       ),
     );
   }
