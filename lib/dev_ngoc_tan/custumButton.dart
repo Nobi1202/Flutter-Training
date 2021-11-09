9e27be72c9a544c4352d329c6e927ae3ea1845f0
TylerDo <ngoctan2512@gmail.com>
2021-11-09 10:57:32 +0700

    Update task1

---
+++ lib/dev_ngoc_tan/custumButton.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class CustumButton extends  StatelessWidget {
+  const CustumButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child:
+    );
+  }
+}
--- lib/dev_ngoc_tan/main_screen.dart
+++ lib/dev_ngoc_tan/main_screen.dart
@@ -3,14 +3,108 @@ import 'package:flutter/material.dart';
 class MainScreen extends StatelessWidget {
   const MainScreen({Key? key}) : super(key: key);
 
+  get children => null;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Dev Ngoc Tuan Screen"),
-      ),
-      body: const Center(
-        child: Text("duoc rồi nhé em"),
+      //appBar: AppBar(
+      //  title: const Text("Dev Ngoc Tuan Screen"),
+      // ),
+      body: Padding(
+        padding: const EdgeInsets.all(13),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(height: 50),
+              Text(
+                'Collection of Greenify template',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+              ),
+              SizedBox(height: 40),
+              ElevatedButton(
+                onPressed: () {},
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Template 1',
+                    style: TextStyle(fontSize: 30),
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12), // <-- Radius
+                  ),
+                ),
+              ),
+              SizedBox(height: 40),
+              ElevatedButton(
+                onPressed: () {},
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Template 2',
+                    style: TextStyle(fontSize: 30),
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12), // <-- Radius
+                  ),
+                ),
+              ),
+              SizedBox(height: 40),
+              ElevatedButton(
+                onPressed: () {},
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Template 3',
+                    style: TextStyle(fontSize: 30),
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12), // <-- Radius
+                  ),
+                ),
+              ),
+              SizedBox(height: 40),
+              ElevatedButton(
+                onPressed: () {},
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Template 4',
+                    style: TextStyle(fontSize: 30),
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12), // <-- Radius
+                  ),
+                ),
+              ),
+              SizedBox(height: 40),
+              ElevatedButton(
+                onPressed: () {},
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Template 5',
+                    style: TextStyle(fontSize: 30),
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12), // <-- Radius
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
