bf64e9d05a41fd134438869f9a5b08ed8def4715
saobang001111@gmail.com <you@example.com>
2021-11-09 16:34:19 +0700

    update

--- lib/dev_ngoc_tan/main_screen.dart
+++ lib/dev_ngoc_tan/main_screen.dart
@@ -4,8 +4,6 @@ import 'package:flutter_traning_chill/dev_ngoc_tan/custom_button.dart';
 class MainScreen extends StatelessWidget {
   const MainScreen({Key? key}) : super(key: key);
 
-  get children => null;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
